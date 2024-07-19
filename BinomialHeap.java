42e219fd64c88e1bb485264bdf8aa77d17e61e4a
Noga Arian <nogaarian@mail.tau.ac.il>
2024-07-19 21:19:30 +0300

    meld updates

--- BinomialHeap.java
+++ BinomialHeap.java
@@ -10,19 +10,22 @@ public class BinomialHeap
 	public HeapNode last;
 	public HeapNode min;
 	
-	//constructor of BinomialHeap: @noga
+	
+	//constructor of BinomialHeap:
 	
 	public BinomialHeap() {
 		size = 0;
 		last=null;
 		min=null;
 	}
+	
 	/*
 	public BinomialHeap(HeapNode node) {
 		size = node.getRank()+1;
 		last = node;
 		min = node;	
-	}*/
+	}
+	*/
 
 	/**
 	 * 
@@ -35,13 +38,13 @@ public class BinomialHeap
 	 */
 	public HeapItem insert(int key, String info) 
 	{   
-		BinomialHeap heap2 = new BinomialHeap(); //creat new heap from new HeapNode
+		BinomialHeap heap2 = new BinomialHeap(); //Create new heap from new HeapNode
 		HeapNode newNode = new HeapNode(key, info);
 		heap2.size = 1;
 		heap2.last = newNode;
 		heap2.min = newNode;
 		this.meld(heap2);
-		return newNode.item; // should return this?
+		return newNode.item;
 	}
 
 	/**
@@ -96,8 +99,7 @@ public class BinomialHeap
 	 */
 	public void meld(BinomialHeap heap2)
 	{
-		//TODO
-		//in case heap is empty @noga
+		//in case heap is empty
 		if (this.size==0){
 			this.size = heap2.size;
 			this.last= heap2.last;
@@ -105,58 +107,204 @@ public class BinomialHeap
 			return;
 		}
 		
-		//key<min? is so
-		//in case heap is not empty @noga
-		else {
-			for 
 
-			//if we have 2 trees of the same degree
-			
-			//otherwise we add the relevant Bi to the heap
-			
+		//in case heap is not empty 
+		else {
+			int[] originalArray = reverseArray(ToBinaryArray(this.size));
+			int[] newArray =reverseArray(ToBinaryArray(heap2.size));
+			int minLen = Math.min(originalArray.length, newArray.length);
+			HeapNode prevOriTree = this.last;
+			HeapNode curOriTree = prevOriTree.next;
+			HeapNode curNewTree = heap2.last.next;
+			HeapNode carry = null; 	//carry is a root
+			if (curOriTree.getKey()>curNewTree.getKey() && this.min==curOriTree) { //update min pointer @noga
+				this.min =curNewTree;
+			}
+			for (int i=0; i<minLen;i++) {
+			if(carry==null) {          		//no carry
+				if (newArray[i]==0 && originalArray[i]==1) {
+					prevOriTree = prevOriTree.next;
+					curOriTree = curOriTree.next;
+				}
+				else if (newArray[i]==1 && originalArray[i]==0) {
+					this.simpleJoin(curNewTree, curOriTree,prevOriTree);
+					curNewTree = curNewTree.next;
+					//originalArray[i]=1;
+				}
+				else if (newArray[i]==1 && originalArray[i]==1) {
+					carry=this.ComplexJoin(curOriTree, curNewTree, curOriTree.next, prevOriTree);
+					prevOriTree = prevOriTree.next;
+					curOriTree = curOriTree.next;
+					curNewTree = curNewTree.next;
+					originalArray[i+1]=1;
+				}
+			}
+				else { 						//carry=1
+					if (newArray[i]==0 && originalArray[i]==0) {
+						this.simpleJoin(carry, curOriTree, prevOriTree);
+						carry=null;
+					}
+					else if (newArray[i]==0 && originalArray[i]==1) {
+						carry=this.ComplexJoin(curOriTree, carry, curOriTree.next, prevOriTree);
+						prevOriTree = prevOriTree.next;
+						curOriTree = curOriTree.next;
+						originalArray[i+1]=1;
+					}
+					else if (newArray[i]==1 && originalArray[i]==0) {
+						carry = this.hangHeapCarry(curNewTree, carry);
+						curNewTree = curNewTree.next;
+						originalArray[i+1]=1;
+						
+					}
+					else if (newArray[i]==1 && originalArray[i]==1) {
+						if (curOriTree.getKey() <= carry.getKey() && curOriTree.getKey() <= curNewTree.getKey()) { //original key is the smallest
+							carry = this.hangHeapCarry(curNewTree, carry);
+							prevOriTree = prevOriTree.next;
+							curOriTree = curOriTree.next;
+							curNewTree = curNewTree.next;
+							originalArray[i+1]=1;
+						}
+						else if(curOriTree.getKey() > carry.getKey() && carry.getKey() < curNewTree.getKey()) { //carry key is the smallest
+							
+							carry.next = curOriTree.next;
+							prevOriTree.next= carry;
+							curOriTree.next = null;
+							carry = this.hangHeapCarry(curNewTree, curOriTree);
+							prevOriTree = prevOriTree.next;
+							curOriTree = curOriTree.next;
+							curNewTree = curNewTree.next;
+							originalArray[i+1]=1;
+							
+						}
+						
+						else if(curNewTree.getKey() < carry.getKey() && curOriTree.getKey() > curNewTree.getKey()) { //newTree key is the smallest
+						//TODO
+						}
+					}
+				}
+				
+				//if the arrays are not the same size -> continue on the change
+			}
 		return;
 		}
 	}
 	
-	//original [1,1,0,1] 
-	//insert[1]
-	   //heap2 [1,1,0,0]
-	//carry [0,1,0,0]
-	//we should use recursion/loop to jump from parent to parent in the first level and decide where to meld 2 branches
-	//how to use the external method?
 	
-	private BinomialHeap joinSameDegree(BinomialHeap heap2) {
-		//TODO
-		return null; //need to return the new root?
+	//example
+	//original [0,0,1,0] 
+	//insert[1,1]
+	//carry [1,0,0,0]	
+	
+	/**
+	 * 
+	 * Get an array of 0,1 which represents binary number @noga
+	 * Return the reversed array
+	 * Complexity: O(n)
+	 *   
+	 */
+	
+	private static int[] reverseArray (int[] array) { //@noga
+		int lenArray= array.length;
+		int[] newArray = new int[lenArray];
+		for (int i=0; i<lenArray; i++) {
+			newArray[i] = array[lenArray-1-i];
+		}
+		return newArray;
 	}
+	
+	
+	/**
+	 * 
+	 *
+	 *   
+	 */
 	
 	//0+1 , 1+0
-	private binomialHeap simpleJoin () {
+	private void simpleJoin (HeapNode root, HeapNode nodeNext, HeapNode nodePrev) {
 		
+		if (root.getKey() < this.min.getKey()) {
+			this.min = root;
+		}
+		nodePrev.next = root;
+		root.next= nodeNext;
 	}
-	//1+1 when its next is 0
-	   //call to meldSameDegree and call to simple join and put 1
 	
 	
-	//1+1 when its next is 1
+	/**
+	 * 
+	 * 
+	 *   
+	 */
 	
-	//carry is a tree
-	//חיבור בינארי
+	//1+1
+	private HeapNode ComplexJoin (HeapNode originalRoot,HeapNode newRoot, HeapNode nodeNext, HeapNode nodePrev) {
 		
+		if (originalRoot.getKey() < newRoot.getKey()) { //simpler case - the original root is the 
+			hangHeap (originalRoot, newRoot);
+			return originalRoot;
+		}
+		
+		else {
+			
+			if(this.min == originalRoot) {
+				this.min = newRoot;
+			}
+			
+			nodePrev.next = newRoot;
+			newRoot.next = originalRoot.next;
+			hangHeap (newRoot,originalRoot);
+			return newRoot;
+		}
+	}
 	
 	
-	private static int[] sizeToBinaryArray(int size) {
+	/**
+	 * 
+	 * 
+	 *   
+	 */
+	
+	private HeapNode hangHeap(HeapNode topRoot, HeapNode lowRoot) {
+		lowRoot.next = topRoot.child.next;
+		topRoot.child.next = lowRoot;
+		topRoot.child = lowRoot;
+		lowRoot.parent = topRoot;
+		topRoot.rank += lowRoot.rank;
+		return topRoot;
+	}
+	
+	/**
+	 * 
+	 * 
+	 *   
+	 */
+	
+	private HeapNode hangHeapCarry(HeapNode root1, HeapNode root2) {
+		HeapNode topRoot = root1;
+		HeapNode lowRoot = root2;
+		if (root1.getKey() > root2.getKey()) {
+			topRoot = root2;
+			lowRoot = root1;
+		}
+		return hangHeap(topRoot, lowRoot);
+	}
+	
+	/**
+	 * 
+	 * 
+	 *   
+	 */
+	
+	private static int[] ToBinaryArray(int size) {
 		String binary = Integer.toBinaryString(size);
 		int n = binary.length();
-		int[] BinaryArray = new int[n];
+		int[] BinaryArray = new int[n+1];
 		for (int i=0; i<n; i++) {
-			BinaryArray[i]= binary.charAt(i); //list of string of 1,0
+			BinaryArray[i]= binary.charAt(i) - '0';
 		}
+		BinaryArray[n]=0;
 		return BinaryArray; 
 	}
-	
-	
-	
 	
 
 	/**
@@ -201,11 +349,11 @@ public class BinomialHeap
 		public HeapNode parent;
 		public int rank;
 		
-		//constructor HeapNode: @noga
+		//constructor HeapNode:
 		public HeapNode(int key, String info) {
 			item = new HeapItem(this, key, info);
 			child = null;
-			next = this; //because the next of B0 is itself
+			next = this;
 			parent = null;
 			rank = 0;
 		}
@@ -227,7 +375,10 @@ public class BinomialHeap
 			return this.rank;	
 		}
 		
-		//TODO ?
+		public int getKey() {
+			return this.item.key;
+		}
+		
 		
 		/**
 		 * setters
@@ -237,7 +388,7 @@ public class BinomialHeap
 		private void setChild(HeapNode child) {
 			this.child= child;
 		}
-		//TODO all	
+		//TODO	
 	}
 
 	/**
@@ -249,7 +400,7 @@ public class BinomialHeap
 		public int key;
 		public String info;
 		
-		//constructor HeapItem: @noga
+		//constructor HeapItem:
 		public HeapItem(HeapNode node, int key, String info) {
 			this.node = node;
 			this.key = key;
